2909bccd633627f16abc02349cfd30be17cf9d7b
Jeroen Dunselman <jeroduns@gmail.com>
2019-01-26 13:33:02 +0100

    Add new map for itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,12 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Jeroen Dunselman on 26/01/2019.
+//  Copyright © 2019 Jeroen Dunselman. All rights reserved.
+//
+
+import Foundation
+class MapForItinerary: NSObject {
+    
+}
